9fc1659903604c1b53178cc1017da70e8dda9b42
Anirudh G Savanur <105296517+anirudhgs1106@users.noreply.github.com>
2023-12-22 14:14:46 +0530

    Delete 8. DC Motor directory

--- 8. DC Motor/dc_motor.d
+++
@@ -1,7 +0,0 @@
-.\dc_motor.o: DC_motor.c
-.\dc_motor.o: C:\Keil\ARM\Inc\NXP\LPC17xx\lpc17xx.h
-.\dc_motor.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
-.\dc_motor.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\dc_motor.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
-.\dc_motor.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
-.\dc_motor.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
